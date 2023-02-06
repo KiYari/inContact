2981851e2c70e2b9c52d4587982f5b04e36a4542
KiYari <termrdz@gmail.com>
2023-02-06 10:10:51 +1000

    initialization

---
+++ back/inContact/src/main/java/com/gruppa/inContact/InContactApplication.java
@@ -0,0 +1,13 @@
+package com.gruppa.inContact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InContactApplication.class, args);
+	}
+
+}
---
+++ back/inContact/src/test/java/com/gruppa/inContact/InContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruppa.inContact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
